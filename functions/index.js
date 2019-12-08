36eb3f8eba178a9bd1b398c85994ddadfcda2f26
Anton.Smolin <antonlezer@gmail.com>
2019-12-08 21:59:11 +0200

    Add function notification likes, comment and remove notification in unlikes scream

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,8 @@ const functions = require('firebase-functions');
 const app = require('express')();
 const FBAuth = require('./util/fbAuth');
 
+const db = require('./util/admin');
+
 const { getAllScreams, postOneScream, getScream, commentOnScream, likeScream, unlikeScream, deleteScream } = require('./handles/screams');
 const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser} = require('./handles/users');
 
@@ -22,4 +24,67 @@ app.post('/user', FBAuth, addUserDetails);
 app.post('/user', FBAuth, getAuthenticatedUser);
 
 
-exports.api = functions.https.onRequest(app);
+exports.api = functions.region('europe-west1').https.onRequest(app);
+
+exports.createNotificationOnLike = functions.region('europe-west1').firestore.document('likes/{id}')
+.onCreate((snapshot)  => {
+  db.doc(`/scream/${snapshot.data().screamId}`).get()
+    .then(doc => {
+      if(doc.exists){
+        return db.doc(`/notifications/${snapshot.id}`).set({
+          createAt: new Date().toISOString(),
+          recipient: doc.data().userHandle,
+          sender: snapshot.data().userHandle,
+          type: 'like',
+          read: false,
+          screamId: doc.id
+        })
+      }
+    })
+    .then(() => {
+      return;
+    })
+    .catch((err) => {
+      console.error(err);
+      return;
+    })
+});
+
+exports.createNotificationOnComment = functions.region('europe-west1').firestore.document('comment/{id}')
+  .onCreate((snapshot)  => {
+    db.doc(`/scream/${snapshot.data().screamId}`).get()
+      .then(doc => {
+        if(doc.exists){
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'comment',
+            read: false,
+            screamId: doc.id
+          })
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      })
+  });
+
+exports.deleteNotificationOnLikes = functions
+  .region('europe-west1')
+  .firestore.document('likes/{id}')
+  .onDelete((snapshot)  => {
+    db.doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .then(() => {
+        return
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      })
+  });
